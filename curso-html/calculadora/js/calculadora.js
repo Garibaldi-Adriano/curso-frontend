956422e8ed4d407f51e7906ae6db0a34e696b242
Garibaldi Adriano Neto <Garibaldi_Adriano@live.com>
2024-08-29 15:06:20 +0000

    Atualiacao de trabalhos

---
+++ curso-html/calculadora/js/calculadora.js
@@ -0,0 +1,32 @@
+btnCalcular = document.getElementById('calcular')
+
+function calculadora(){
+    let numero1 = Number(document.getElementById('valor1.value'))
+    let numero2 = Number(document.getElementById('valor2.value'))
+    let opcao = document.getElementById('operacao').value
+    let resposta = 0;
+
+    switch(opcao){
+        case 'soma':
+            resposta = numero1 + numero2;
+            break;
+        case 'subtracao':
+            resposta = numero1 - numero2;
+            break;
+        case 'multiplicacao':
+            resposta = numero1 * numero2;
+            break;
+        case 'divisao':
+            resposta = numero1 / numero2;
+            break;
+        case 'resto':
+            resposta = numero1 % numero2;
+            break;
+        default:
+            break;
+    }
+    document.getElementById('resultado').textContent = String(resposta)
+}
+
+
+btnCalcular.addEventListner('click',calculadora)
